6dc2cfeb0e27e59533a6b350112df6749d0f4600
Abhishek Jena <iamabhishekjena@gmail.com>
2023-10-01 20:06:41 +0530

    abhishek.jena| solution 33| solved leetcode question 33 using binary search

---
+++ src/Ques33.java
@@ -0,0 +1,32 @@
+public class Ques33 {
+    public int search(int[] nums, int target) {
+        int l = 0;
+        int r = nums.length-1;
+        int mid;
+
+        while(l<=r){
+            mid = l + (r-l)/2;
+
+            if (nums[mid]==target)
+                return mid;
+            else if(nums[mid]>=nums[l]){
+                if(nums[l]<=target && nums[mid]> target){
+                    r =mid-1;
+                }
+                else{
+                    l = mid +1;
+                }
+            }
+            else{
+                if(target<=nums[r] && nums[mid]<target){
+                    l =mid+1;
+                }
+                else{
+                    r =mid -1;
+                }
+            }
+
+        }
+        return -1;
+    }
+}
